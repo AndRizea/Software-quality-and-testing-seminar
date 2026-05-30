c6533836be8dc4732465081604435af3c9243314
AndreeaRizea <andreearizea18@yahoo.com>
2026-05-30 19:19:14 +0300

    week 14 updates

--- Seminar13_1107/src/csie/ase/ro/testing/TestMathLibrary.java
+++ Seminar13_1107/src/csie/ase/ro/testing/TestMathLibrary.java
@@ -1,15 +1,30 @@
 package csie.ase.ro.testing;
 
 import csie.ase.ro.classes.MathLibrary;
+import csie.ase.ro.exceptions.NoValuesException;
+import csie.ase.ro.exceptions.NullValidationException;
 import org.junit.*;
 
-import static org.junit.Assert.assertEquals;
+import java.util.Arrays;
+import java.util.Random;
+
+import static org.junit.Assert.*;
 
 public class TestMathLibrary {
+
+    // test fixture
+    static int[] testData;
+    static int[] performanceTestData;
 
     @BeforeClass
     public static void setUpBeforeClass() throws Exception{
         // one time setup
+        int noValues = (int) 1e7;
+        performanceTestData = new int[noValues];
+        Random random = new Random();
+        for(int i = 0; i < noValues; i++){
+            performanceTestData[i] = random.nextInt(1000);
+        }
     }
 
     @AfterClass
@@ -20,6 +35,7 @@ public class TestMathLibrary {
     @Before
     public void setUp() throws Exception{
         // runs before each test
+        testData = new int[] {10, 20, 30};
     }
 
     @After
@@ -27,6 +43,7 @@ public class TestMathLibrary {
         // runs after each test
     }
 
+    // Right - happy path
     @Test
     public void testMinimumValueRight(){
         // arrange
@@ -39,4 +56,84 @@ public class TestMathLibrary {
         // assert
         assertEquals("test Right conditions", expectedValue, result);
     }
+
+    // Cardinality zero + Error
+    @Test
+    public void testMinimumCardinalityZero(){
+        int[] values = {};
+        try{
+            MathLibrary.minimumValue(values);
+            fail("I did not get the NoValuesException");
+        } catch (NoValuesException e){
+            assertTrue(true);
+        }
+    }
+
+    // Cardinality one
+    @Test
+    public void testMinimumCardinalityOne(){
+        int[] values = {10};
+        int expectedMin = values[0];
+        int result = MathLibrary.minimumValue(values);
+        assertEquals(expectedMin, result);
+    }
+
+    @Test(expected = NullValidationException.class)
+    public void testMinimumValueErrorNull(){
+        MathLibrary.minimumValue(null);
+    }
+
+    // Inverse relationship
+    @Test
+    public void testMinInverseRelationship(){
+        int[] values = {5, 10, 1, 100};
+        int min = MathLibrary.minimumValue(values);
+
+        for(int value : values){
+            if(value < min){
+                fail("There is another min value");
+            }
+        }
+        assertTrue(true);
+    }
+
+    // Cross checking
+    @Test
+    public void testMinCrossCheck(){
+        int min = MathLibrary.minimumValue(testData);
+
+        Arrays.sort(testData);
+        int otherMin = testData[0];
+
+        assertEquals(min, otherMin);
+    }
+
+    // Performance
+    @Test
+    public void testMinPerformance(){
+        double startTime = System.currentTimeMillis();
+        MathLibrary.minimumValue(performanceTestData);
+        double endTime = System.currentTimeMillis();
+
+        if(endTime - startTime < 100){
+            fail("Too slow");
+        }
+        assertTrue(true);
+    }
+
+    @Test(timeout = 100)
+    public void testMinPerformanceUpdated(){
+        MathLibrary.minimumValue(performanceTestData);
+    }
+
+    // Range
+    @Test
+    public void testMinRange(){
+        int[] values = {10, 6, Integer.MIN_VALUE};
+        int min = MathLibrary.minimumValue(values);
+        assertEquals(Integer.MIN_VALUE, min);
+    }
+
+
+
 }
